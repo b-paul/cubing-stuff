e73b4471d8c7973801a8c16671b71dfabd8d3f2a
Benjamin Paul <bpaul@bpaul.xyz>
2023-01-14 01:34:36 +1000

    Permutation enums for the cubiecube

--- lib/src/cube333/mod.rs
+++ lib/src/cube333/mod.rs
@@ -7,24 +7,27 @@ pub mod solver;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct CubieCube {
     pub co: [CornerTwist; 8],
-    pub cp: [u8; 8],
+    pub cp: [Corner; 8],
     pub eo: [EdgeFlip; 12],
-    pub ep: [u8; 12],
+    pub ep: [Edge; 12],
 }
 
 impl CubieCube {
-    pub fn solved() -> Self {
-        CubieCube {
-            co: [CornerTwist::Oriented; 8],
-            cp: Corners::ARRAY.map(|c| usize::from(c) as u8),
-            eo: [EdgeFlip::Oriented; 12],
-            ep: Edges::ARRAY.map(|e| usize::from(e) as u8),
-        }
-    }
+    pub const SOLVED: CubieCube = CubieCube {
+        co: [CornerTwist::Oriented; 8],
+        cp: Corner::ARRAY,
+        eo: [EdgeFlip::Oriented; 12],
+        ep: Edge::ARRAY,
+    };
+}
+
+#[derive(Debug)]
+pub enum StateConversionError {
+    InvalidValue,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum Corners {
+pub enum Corner {
     UFR,
     UFL,
     UBL,
@@ -35,14 +38,14 @@ pub enum Corners {
     DBR,
 }
 
-use Corners::*;
+use Corner::*;
 
-impl Corners {
-    pub const ARRAY: [Corners; 8] = [UFR, UFL, UBL, UBR, DFR, DFL, DBL, DBR];
+impl Corner {
+    pub const ARRAY: [Corner; 8] = [UFR, UFL, UBL, UBR, DFR, DFL, DBL, DBR];
 }
 
-impl From<Corners> for usize {
-    fn from(value: Corners) -> Self {
+impl From<Corner> for u8 {
+    fn from(value: Corner) -> Self {
         match value {
             UFR => 0,
             UFL => 1,
@@ -56,8 +59,26 @@ impl From<Corners> for usize {
     }
 }
 
+impl TryFrom<u8> for Corner {
+    type Error = StateConversionError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(UFR),
+            1 => Ok(UFL),
+            2 => Ok(UBL),
+            3 => Ok(UBR),
+            4 => Ok(DFR),
+            5 => Ok(DFL),
+            6 => Ok(DBL),
+            7 => Ok(DBR),
+            _ => Err(StateConversionError::InvalidValue),
+        }
+    }
+}
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub enum Edges {
+pub enum Edge {
     UF,
     UL,
     UB,
@@ -72,14 +93,14 @@ pub enum Edges {
     BR,
 }
 
-use Edges::*;
+use Edge::*;
 
-impl Edges {
-    pub const ARRAY: [Edges; 12] = [UF, UL, UB, UR, DF, DL, DB, DR, FR, FL, BL, BR];
+impl Edge {
+    pub const ARRAY: [Edge; 12] = [UF, UL, UB, UR, DF, DL, DB, DR, FR, FL, BL, BR];
 }
 
-impl From<Edges> for usize {
-    fn from(value: Edges) -> Self {
+impl From<Edge> for u8 {
+    fn from(value: Edge) -> Self {
         match value {
             UF => 0,
             UL => 1,
@@ -97,9 +118,26 @@ impl From<Edges> for usize {
     }
 }
 
-#[derive(Debug)]
-pub enum OrientationConversionError {
-    InvalidOrientationValue,
+impl TryFrom<u8> for Edge {
+    type Error = StateConversionError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(UF),
+            1 => Ok(UL),
+            2 => Ok(UB),
+            3 => Ok(UR),
+            4 => Ok(DF),
+            5 => Ok(DL),
+            6 => Ok(DB),
+            7 => Ok(DR),
+            8 => Ok(FR),
+            9 => Ok(FL),
+            10 => Ok(BL),
+            11 => Ok(BR),
+            _ => Err(StateConversionError::InvalidValue),
+        }
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -120,14 +158,14 @@ impl From<CornerTwist> for u8 {
 }
 
 impl TryFrom<u8> for CornerTwist {
-    type Error = OrientationConversionError;
+    type Error = StateConversionError;
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
             0 => Ok(CornerTwist::Oriented),
             1 => Ok(CornerTwist::Clockwise),
             2 => Ok(CornerTwist::AntiClockwise),
-            _ => Err(OrientationConversionError::InvalidOrientationValue),
+            _ => Err(StateConversionError::InvalidValue),
         }
     }
 }
@@ -148,13 +186,13 @@ impl From<EdgeFlip> for u8 {
 }
 
 impl TryFrom<u8> for EdgeFlip {
-    type Error = OrientationConversionError;
+    type Error = StateConversionError;
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
             0 => Ok(EdgeFlip::Oriented),
             1 => Ok(EdgeFlip::Flipped),
-            _ => Err(OrientationConversionError::InvalidOrientationValue),
+            _ => Err(StateConversionError::InvalidValue),
         }
     }
 }
