75aa5eca712b5ebd1823e50adf2b548bdaca9c84
Benjamin Paul <bpaul@bpaul.xyz>
2024-01-02 09:43:53 +1000

    Update the conversion benchmark to use the new library (conversion seems to be sufficiently fast)

--- cube-lib/benches/conversion.rs
+++ cube-lib/benches/conversion.rs
@@ -1,10 +1,10 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use lib::cube333::coordcube::CoordCube;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use cube_lib::cube333::coordcube::CoordCube;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("double convert identity", |b| b.iter(|| {
-        let identity = CoordCube.solved();
-        identity.to_cubie().to_coord()
+        let identity = CoordCube::SOLVED;
+        black_box(identity.to_cubie().to_coord())
     }));
 }
 
