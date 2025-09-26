a8f5d351a6187203a1b72d24f11569bb24be8a75
bpaul <bpaul@bpaul.xyz>
2025-09-26 18:03:43 +1000

    Reorganise

--- pin-orders/src/z12.rs
+++ pin-orders/src/z12.rs
@@ -2,19 +2,7 @@
 // We work over R = Z_12 since this is a PIR! Yay!
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct Z12(i8);
-
-impl Z12 {
-    fn is_unit(self) -> bool {
-        [1, 5, 7, 11].contains(&self.0)
-    }
-}
-
-impl std::fmt::Display for Z12 {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", (self.0 + 5).rem_euclid(12) - 5)
-    }
-}
+pub struct Z12(i8);
 
 use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
 
@@ -22,7 +10,7 @@ impl Add for Z12 {
     type Output = Z12;
 
     fn add(self, rhs: Self) -> Self::Output {
-        Z12((self.0 + rhs.0).rem_euclid(12))
+        Z12::new(self.0 + rhs.0)
     }
 }
 
@@ -36,7 +24,7 @@ impl Sub for Z12 {
     type Output = Z12;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        Z12((self.0 - rhs.0).rem_euclid(12))
+        Z12::new(self.0 - rhs.0)
     }
 }
 
@@ -50,7 +38,7 @@ impl Mul for Z12 {
     type Output = Z12;
 
     fn mul(self, rhs: Self) -> Self::Output {
-        Z12((self.0 * rhs.0).rem_euclid(12))
+        Z12::new(self.0 * rhs.0)
     }
 }
 
@@ -60,36 +48,52 @@ impl MulAssign for Z12 {
     }
 }
 
-/// Returns g, s, t, u, v such that sv - tu is a unit, sa + tb = g and ua + vt = 0.
-fn gcdex(a: Z12, b: Z12) -> (Z12, Z12, Z12, Z12, Z12) {
-    // code from https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Pseudocode lol
-
-    let (mut old_r, mut r) = (a.0, b.0);
-    let (mut old_s, mut s) = (1, 0);
-    let (mut old_t, mut t) = (0, 1);
-
-    while r != 0 {
-        let q = old_r / r;
-        (old_r, r) = (r, old_r - q * r);
-        (old_s, s) = (s, old_s - q * s);
-        (old_t, t) = (t, old_t - q * t);
+impl Z12 {
+    pub fn new(x: i8) -> Z12 {
+        Z12(x.rem_euclid(12))
     }
 
-    (
-        Z12(old_r.rem_euclid(12)),
-        Z12(old_s.rem_euclid(12)),
-        Z12(old_t.rem_euclid(12)),
-        Z12(s.rem_euclid(12)),
-        Z12(t.rem_euclid(12)),
-    )
+    pub fn is_unit(self) -> bool {
+        [1, 5, 7, 11].contains(&self.0)
+    }
+
+    /// Returns g, s, t, u, v such that sv - tu is a unit, sa + tb = g and ua + vt = 0.
+    pub fn gcdex(self, b: Z12) -> (Z12, Z12, Z12, Z12, Z12) {
+        // code from https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Pseudocode lol
+
+        let (mut old_r, mut r) = (self.0, b.0);
+        let (mut old_s, mut s) = (1, 0);
+        let (mut old_t, mut t) = (0, 1);
+
+        while r != 0 {
+            let q = old_r / r;
+            (old_r, r) = (r, old_r - q * r);
+            (old_s, s) = (s, old_s - q * s);
+            (old_t, t) = (t, old_t - q * t);
+        }
+
+        (
+            Z12::new(old_r),
+            Z12::new(old_s),
+            Z12::new(old_t),
+            Z12::new(s),
+            Z12::new(t),
+        )
+    }
+}
+
+impl std::fmt::Display for Z12 {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", (self.0 + 5).rem_euclid(12) - 5)
+    }
 }
 
 #[test]
 fn z12_tests() {
     for i in 0..12 {
         for j in 0..12 {
-            let (a, b) = (Z12(i), Z12(j));
-            let (g, s, t, u, v) = gcdex(a, b);
+            let (a, b) = (Z12::new(i), Z12::new(j));
+            let (g, s, t, u, v) = a.gcdex(b);
             assert!((s * v - t * u).is_unit());
             assert_eq!(s * a + t * b, g);
             assert_eq!(u * a + v * b, Z12(0));
